009b07de8bbb4cd16008338fc78c187576876e99
KamranSaleemi <asaleemi@360viewtechnologies.com>
2019-01-28 18:08:26 -0600

    .

--- DevopsTest/Program.cs
+++ DevopsTest/Program.cs
@@ -17,6 +17,7 @@ namespace DevopsTest
             CreateWebHostBuilder(args).Build().Run();
         }
 
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
